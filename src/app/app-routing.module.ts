7f1508d8dc8ba400d317e637224a2ca3b57f1803
aevilesaguiar <aeviles@hotmail.com>
2022-09-06 11:32:38 -0300

    rota para Component Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 
 const routes: Routes = [
 
   //quando o meu path for vazio eu renderizo com NavComponent
   {
-    path:'', component:NavComponent
+    path:'', component:NavComponent, children:[
+      {path: 'home', component: HomeComponent} //children significa filha , rota filha do componente nav
+    ]
 }
 ];
 
