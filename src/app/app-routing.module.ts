2ef6365fb0227c8977b02b8c4a209d646f586874
aevilesaguiar <aeviles@hotmail.com>
2022-09-06 15:02:37 -0300

    rota para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 //criando a rota
 const routes: Routes = [
+  { path:'login', component:LoginComponent},
 
   //quando o meu path for vazio eu renderizo com NavComponent
   {
